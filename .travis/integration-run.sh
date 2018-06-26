68a9b9d4c863a7c72ad8339e6a36caf634cf7149
Adam Bozanich <adam.boz@gmail.com>
2018-06-26 15:22:41 -0700

    checkpt

--- .travis/integration-run.sh
+++ .travis/integration-run.sh
@@ -5,11 +5,11 @@ set -exo pipefail
 export DOMAIN=127.0.0.1.nip.io
 
 (
-  cd $(dirname $0)../_run/multi
+  cd $(dirname $0)/../_run/multi
   make install-all-baremetal
 )
 
 (
-  cd $(dirname $0)../_integration
+  cd $(dirname $0)/../_integration
   make kube-run
 )
